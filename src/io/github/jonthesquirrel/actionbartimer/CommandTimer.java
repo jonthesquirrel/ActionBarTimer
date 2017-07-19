ea15fd25d87921080102d6b05c41abdec39983c1
Jonathan Herman <jdh11235@gmail.com>
2017-07-19 04:46:29 -0500

    add ActionBarAPI depend, test command

--- src/io/github/jonthesquirrel/actionbartimer/CommandTimer.java
+++ src/io/github/jonthesquirrel/actionbartimer/CommandTimer.java
@@ -1,8 +1,11 @@
 package io.github.jonthesquirrel.actionbartimer;
 
+import com.connorlinfoot.actionbarapi.ActionBarAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandTimer implements CommandExecutor {
 
@@ -10,6 +13,9 @@ public class CommandTimer implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length > 0) {
             if (args[0].equalsIgnoreCase("set")) {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    ActionBarAPI.sendActionBar(player,"test", 20);
+                }
                 return true;
             }
             if (args[0].equalsIgnoreCase("clear")) {
