a1188c695f0d3435e0611ac486f162b3612bea38
Jonathan Herman <jdh11235@gmail.com>
2017-07-19 03:09:16 -0500

    Initial commit

---
+++ src/io/github/jonthesquirrel/actionbartimer/Main.java
@@ -0,0 +1,17 @@
+package io.github.jonthesquirrel.actionbartimer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
