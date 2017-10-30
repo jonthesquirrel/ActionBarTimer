6a22207d0d18bdd901fb92fed8d9fc73cbfac84c
Jonathan Herman <jdh11235@gmail.com>
2017-10-30 04:15:04 -0500

    catch bad input

--- src/main/java/io/github/jonthesquirrel/actionbartimer/CommandTimer.java
+++ src/main/java/io/github/jonthesquirrel/actionbartimer/CommandTimer.java
@@ -4,6 +4,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import java.time.Duration;
+import java.time.format.DateTimeParseException;
 
 public class CommandTimer implements CommandExecutor {
 
@@ -11,17 +12,31 @@ public class CommandTimer implements CommandExecutor {
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (args.length > 0) {
             if (args[0].equalsIgnoreCase("set")) {
-                AsyncTimer.setTime(Formatter.parse(args[1]));
-                //TODO return false if formatter fails to parse string (bad format)?
-                return true;
+                try {
+                    Duration d = Formatter.parse(args[1]);
+                    AsyncTimer.setTime(d);
+                    return true;
+                } catch (DateTimeParseException e) {
+                    return false;
+                }
             }
             if (args[0].equalsIgnoreCase("add")) {
-                AsyncTimer.addTime(Formatter.parse(args[1]));
-                return true;
+                try {
+                    Duration d = Formatter.parse(args[1]);
+                    AsyncTimer.addTime(d);
+                    return true;
+                } catch (DateTimeParseException e) {
+                    return false;
+                }
             }
             if (args[0].equalsIgnoreCase("remove")) {
-                AsyncTimer.removeTime(Formatter.parse(args[1]));
-                return true;
+                try {
+                    Duration d = Formatter.parse(args[1]);
+                    AsyncTimer.removeTime(d);
+                    return true;
+                } catch(DateTimeParseException e) {
+                    return false;
+                }
             }
             if (args[0].equalsIgnoreCase("start")) {
                 AsyncTimer.start();
